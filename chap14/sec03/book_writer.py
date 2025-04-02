52ed08d2512fb8fe558d99b2f104d3585cfff381
Lee, Sungyong <saintdragon2@gmail.com>
2025-04-03 01:03:42 +0900

    14장 section 이동동

--- chap14/sec03/book_writer.py
+++ chap14/sec03/book_writer.py
@@ -8,7 +8,7 @@ from typing import List
 
 from utils import save_state, get_outline, save_outline 
 from models import Task
-from tools import retrieve, web_search, add_web_pages_json_to_chroma 
+from tools import retrieve  
 from datetime import datetime
 import os 
 
@@ -40,7 +40,6 @@ def supervisor(state: State): # supervisor 에이전트 추가
         supervisor가 활용할 수 있는 agent는 다음과 같다.     
         - content_strategist: 사용자의 요구사항이 명확해졌을 때 사용한다. AI 팀의 콘텐츠 전략을 결정하고, 전체 책의 목차(outline)를 작성한다. 
         - communicator: AI 팀에서 해야 할 일을 스스로 판단할 수 없을 때 사용한다. 사용자에게 진행상황을 사용자에게 보고하고, 다음 지시를 물어본다. 
-        - web_search_agent: 웹 검색을 통해 목차(outline) 작성에 필요한 정보를 확보한다.
         - vector_search_agent: 벡터 DB 검색을 통해 목차(outline) 작성에 필요한 정보를 확보한다.
 
         아래 내용을 고려하여, 현재 해야할 일이 무엇인지, 사용할 수 있는 agent를 단답으로 말하라.
@@ -202,14 +201,11 @@ def content_strategist(state: State):
         이전 대화 내용을 바탕으로 사용자의 요구사항을 분석하고, AI팀이 쓸 책의 세부 목차를 결정한다.
 
         지난 목차가 있다면 그 버전을 사용자의 요구에 맞게 수정하고, 없다면 새로운 목차를 제안한다.
-        목차를 작성하는데 필요한 정보는 "참고 자료"에 있으므로 활용한다. 
 
         --------------------------------
         - 지난 목차: {outline}
         --------------------------------
         - 이전 대화 내용: {messages}
-        --------------------------------
-        - 참고 자료: {references}
         """
     )
 
@@ -222,8 +218,7 @@ def content_strategist(state: State):
     # 입력값 정의
     inputs = {
         "messages": messages,
-        "outline": outline, 
-        "references": state.get("references", {"queries": [], "docs": []})
+        "outline": outline
     }
 
     # 목차 작성
@@ -264,102 +259,6 @@ def content_strategist(state: State):
     return {
         "messages": messages,
         "task_history": task_history
-    }
-
-
-def web_search_agent(state: State): #① (0)
-    print("\n\n============ WEB SEARCH AGENT ============")
-
-    # 작업 리스트 가져와서 web search agent 가 할 일인지 확인하기
-    tasks = state.get("task_history", [])
-    task = tasks[-1]
-
-    if task.agent != "web_search_agent":
-        raise ValueError(f"Web Search Agent가 아닌 agent가 Web Search Agent를 시도하고 있습니다.\n {task}")
-    
-    #③ 시스템 프롬프트 정의
-    web_search_system_prompt = PromptTemplate.from_template(
-        """
-        너는 다른 AI Agent 들이 수행한 작업을 바탕으로, 
-        목차(outline) 작성에 필요한 정보를 웹 검색을 통해 찾아내는 Web Search Agent이다.
-
-        현재 부족한 정보를 검색하고, 복합적인 질문은 나눠서 검색하라.
-
-        - 검색 목적: {mission}
-        --------------------------------
-        - 과거 검색 내용: {references}
-        --------------------------------
-        - 이전 대화 내용: {messages}
-        --------------------------------
-        - 목차(outline): {outline}
-        --------------------------------
-        - 현재 시각 : {current_time}
-        """
-    )
-    
-    #④ 기존 대화 내용 가져오기
-    messages = state.get("messages", [])
-
-    #⑤ 인풋 자료 준비하기
-    inputs = {
-        "mission": task.description,
-        "references": state.get("references", {"queries": [], "docs": []}),
-        "messages": messages,
-        "outline": get_outline(current_path),
-        "current_time": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    }
-
-    #⑥ LLM과 웹 검색 모델 연결
-    llm_with_web_search = llm.bind_tools([web_search])
-
-    #⑦ 시스템 프롬프트와 모델을 연결
-    web_search_chain = web_search_system_prompt | llm_with_web_search
-
-    #⑧ 웹 검색 tool_calls 가져오기
-    search_plans = web_search_chain.invoke(inputs)
-
-    #⑨ 어떤 내용을 검색했는지 담아두기
-    queries = []
-
-    #⑩ 검색 계획(tool_calls)에 따라 검색하기
-    for tool_call in search_plans.tool_calls:
-        print('-------- web search --------', tool_call)
-        args = tool_call["args"]
-        
-        queries.append(args["query"])
-
-        # (10)  검색 결과를 chroma에 추가
-        _, json_path = web_search.invoke(args)
-        print('json_path:', json_path)
-
-        # (10)  JSON 파일을 chroma에 추가
-        add_web_pages_json_to_chroma(json_path)
-
-    #⑪ (11) task 완료
-    tasks[-1].done = True
-    tasks[-1].done_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    #⑪ (11) 새로운 task 추가
-    task_desc = "AI팀이 쓸 책의 세부 목차를 결정하기 위한 정보를 벡터 검색을 통해 찾아낸다."
-    task_desc += f" 다음 항목이 새로 추가되었다\n: {queries}"
-    
-    new_task = Task(
-        agent="vector_search_agent",
-        done=False,
-        description=task_desc,
-        done_at=""
-    )
-
-    tasks.append(new_task)
-
-    #⑫ (12) 작업 후기 메시지
-    msg_str = f"[WEB SEARCH AGENT] 다음 질문에 대한 검색 완료: {queries}"
-    messages.append(AIMessage(msg_str))
-
-    #⑬ (13) state 업데이트
-    return {
-        "messages": messages,
-        "task_history": tasks
     }
 
 
@@ -427,7 +326,6 @@ graph_builder.add_node("supervisor", supervisor)
 graph_builder.add_node("communicator", communicator)
 graph_builder.add_node("content_strategist", content_strategist)
 graph_builder.add_node("vector_search_agent", vector_search_agent)
-graph_builder.add_node("web_search_agent", web_search_agent)
 
 # Edges
 graph_builder.add_edge(START, "supervisor")
@@ -437,12 +335,10 @@ graph_builder.add_conditional_edges(
     {
         "content_strategist": "content_strategist",
         "communicator": "communicator",
-        "vector_search_agent": "vector_search_agent", 
-        "web_search_agent": "web_search_agent"
+        "vector_search_agent": "vector_search_agent"
     }
 )
 graph_builder.add_edge("content_strategist", "communicator")
-graph_builder.add_edge("web_search_agent", "vector_search_agent") #③
 graph_builder.add_edge("vector_search_agent", "communicator")
 graph_builder.add_edge("communicator", END)
 
